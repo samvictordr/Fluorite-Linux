dbe014c23d08c4c9bf42078f4f48a4cc9f1c57d0
Sam Victor <105478252+samvictordr@users.noreply.github.com>
2023-06-18 11:46:29 +0530

    Update main.rs to include VGA buffer and panic handling

--- mesakernel/src/main.rs
+++ mesakernel/src/main.rs
@@ -2,22 +2,25 @@
 
 #![no_std]
 #![no_main]
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"MesaKernel - wip";
+static GREET_FUNC: &[u8] = b"MesaKernel v0.1.0-rc1 - wip";
 /// Mark C to use C calling convention
 /// This function is the entry point!!
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
-
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
+    //vga_buffer::test_output();
+    println!("Hello From mesaKernel v.0.1.0-rc1 {}", "!");
+    panic!("Default Panic");
 
     loop {}
 }
@@ -25,7 +28,7 @@ pub extern "C" fn _start() -> ! {
 /// On-panic function
 #[panic_handler]
 fn panic_handler(_info: &PanicInfo) -> ! {
-
+    println!("{}", info);
     loop {}
 }
 
